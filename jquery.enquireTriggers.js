0c5e6a6803ea00bf1188a5ca437551bfc19ae3bd
W. Ripley <wripley@dwgreen.com>
2016-02-12 14:54:31 -0700

    First JS

---
+++ jquery.enquireTriggers.js
@@ -0,0 +1,24 @@
+/*
+ * jQuery enquireTriggers plug-in 0.1
+ *
+ * https://github.com/littleRm/enquireTriggers
+ *
+ * Copyright (c) 2016 Whitney Ripley
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+
+;(function($) {
+	
+	
+	$.extend($.fn, {
+		enquireTriggers: function(a, b, c) {
+			return true;
+		}
+	});
+	
+	
+})(jQuery);
